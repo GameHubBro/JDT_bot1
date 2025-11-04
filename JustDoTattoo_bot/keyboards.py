367549b6a2fe1b6b4fed4388c2858857ff5b9339
Stanislav Mazalov <Gamehubbro@mail.ru>
2025-11-04 16:58:29 +0100

    Update keyboards.py

--- JustDoTattoo_bot/keyboards.py
+++ JustDoTattoo_bot/keyboards.py
@@ -19,21 +19,11 @@ main_menu = ReplyKeyboardMarkup(
 # --- Клавиатура для раздела “Полезное про тату” ---
 articles_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [
-            KeyboardButton(text="🧠 Как выбрать татуировку"),
-        ],
-        [
-            KeyboardButton(text="💧 Уход за татуировкой"),
-        ],
-        [
-            KeyboardButton(text="🕒 Больно ли делать тату"),
-        ],
-        [
-            KeyboardButton(text="🔗 Все статьи на сайте"),
-        ],
-        [
-            KeyboardButton(text="⬅️ Назад в меню"),
-        ],
+        [KeyboardButton(text="🧠 Как выбрать татуировку")],
+        [KeyboardButton(text="💧 Уход за татуировкой")],
+        [KeyboardButton(text="🕒 Больно ли делать тату")],
+        [KeyboardButton(text="🔗 Все статьи на сайте")],
+        [KeyboardButton(text="⬅️ Назад в меню")],
     ],
     resize_keyboard=True
 )
@@ -41,7 +31,9 @@ articles_kb = ReplyKeyboardMarkup(
 # --- Клавиатура для выбора размера тату ---
 size_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Маленькая"), KeyboardButton(text="Средняя"), KeyboardButton(text="Большая")]
+        [KeyboardButton(text="до 3см"), KeyboardButton(text="от 5см")],
+        [KeyboardButton(text="до 7см"), KeyboardButton(text="до 10см")],
+        [KeyboardButton(text="до 15см"), KeyboardButton(text="от 15см")],
     ],
     resize_keyboard=True
 )
@@ -49,7 +41,9 @@ size_kb = ReplyKeyboardMarkup(
 # --- Клавиатура для выбора количества цветов ---
 colors_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Черно-белая"), KeyboardButton(text="Цветная")]
+        [KeyboardButton(text="один цвет")],
+        [KeyboardButton(text="2 цвета")],
+        [KeyboardButton(text="3 и больше цветов")],
     ],
     resize_keyboard=True
 )
@@ -57,8 +51,11 @@ colors_kb = ReplyKeyboardMarkup(
 # --- Клавиатура для выбора стиля тату ---
 style_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Минимализм"), KeyboardButton(text="Реализм")],
-        [KeyboardButton(text="Графика"), KeyboardButton(text="Надпись")]
+        [KeyboardButton(text="Не знаю")],
+        [KeyboardButton(text="Надпись"), KeyboardButton(text="Графика")],
+        [KeyboardButton(text="Геометрия"), KeyboardButton(text="Абстракция")],
+        [KeyboardButton(text="Реализм"), KeyboardButton(text="Тонкие линии")],
+        [KeyboardButton(text="Блэкворк"), KeyboardButton(text="New School")],
     ],
     resize_keyboard=True
 )
