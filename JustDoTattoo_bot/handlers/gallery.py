25beafd9cc50b584c6037f581d44c691db27a471
Stanislav Mazalov <Gamehubbro@mail.ru>
2025-11-04 16:59:22 +0100

    Update gallery.py

--- JustDoTattoo_bot/handlers/gallery.py
+++ JustDoTattoo_bot/handlers/gallery.py
@@ -1,21 +1,18 @@
+# handlers/gallery.py
 from aiogram import Router, types
-from config import BASE_URL
-from config import IDEAS_URL
+from aiogram.fsm.context import FSMContext
+from config import BASE_URL, IDEAS_URL
 from logger_utils import log_user_action
 
 router = Router()
 
-@router.message(lambda m: m.text == "🎨 Галерея тату")
-async def gallery(message: types.Message):
-    await state.clear() 
+@router.message(lambda m: m.text and "галерея" in m.text.lower())
+async def gallery(message: types.Message, state: FSMContext):
+    # Сбрасываем состояние, если пользователь был в калькуляторе
+    await state.clear()
+
     log_user_action(message.from_user.id, message.from_user.username, "Перешёл в Галерею")
     await message.answer(
         f'Посмотри крутые работы наших мастеров 👇\n{BASE_URL}/gallery\n'
         f'И подписывайся на наш канал "Тату идеи" 👇\n{IDEAS_URL}'
     )
-
-
-
-
-
-
