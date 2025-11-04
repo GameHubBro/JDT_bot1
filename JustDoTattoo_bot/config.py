94f4b6f20eb9e2ba7de3917bd087f4fa5705e4a3
Stanislav Mazalov <Gamehubbro@mail.ru>
2025-11-04 15:29:28 +0100

    Update config.py

--- JustDoTattoo_bot/config.py
+++ JustDoTattoo_bot/config.py
@@ -1,5 +1,6 @@
 BOT_TOKEN = "8125363291:AAFk6cm2AXfwTe3fA8SkUW8A8KJGahhLmHM"
 BASE_URL = "https://justdotattoo.ru"
+IDEAS_URL = "https://t.me/tattooideas_justdotattoo"
 
 ARTICLES_URL = "https://justdotattoo.ru/articles"
 ARTICLE_1_URL = "https://justdotattoo.ru/articles/stoit-li-delat-tatuirovku/"
@@ -9,5 +10,6 @@ ARTICLE_4_URL = "https://justdotattoo.ru/articles/tatuirovki-i-karera/"
 ARTICLE_5_URL = "https://justdotattoo.ru/articles/skolko-chasov-ukhodit-na-sozdanie-tatuirovki/"
 ARTICLE_6_URL = "https://justdotattoo.ru/articles/ukhod-za-tatu-posle-nabitiya-s-plyenkoy/"
 ARTICLE_7_URL = "https://justdotattoo.ru/articles/10-idej-intimnyh-tatuirovok/"
+
 
 STUDIOS_URL = "https://justdotattoo.ru/studios"
