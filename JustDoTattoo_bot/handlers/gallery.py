1ea08b6b050074e78909156331c52288c126d2c3
Stanislav Mazalov <Gamehubbro@mail.ru>
2025-11-04 17:19:20 +0100

    Update gallery.py

--- JustDoTattoo_bot/handlers/gallery.py
+++ JustDoTattoo_bot/handlers/gallery.py
@@ -8,12 +8,13 @@ router = Router()
 
 @router.message(lambda m: m.text and "галерея" in m.text.lower())
 async def gallery(message: types.Message, state: FSMContext):
-    # Сбрасываем состояние, если пользователь был в калькуляторе
     await state.clear()
-
     log_user_action(message.from_user.id, message.from_user.username, "Перешёл в Галерею")
-    await message.answer(
+
+    text = (
         f'Посмотри крутые <a href="{BASE_URL}/gallery">работы наших мастеров</a> 💫\n\n'
         f'И подписывайся на наш <a href="{IDEAS_URL}">канал «Тату идеи»</a> 💫'
     )
 
+    # 👇 обязательно parse_mode="HTML"
+    await message.answer(text, parse_mode="HTML", disable_web_page_preview=True)
