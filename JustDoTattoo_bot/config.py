dcc97d1dde065d1e83c5d7ddbc6a2226727e2b6d
Stanislav Mazalov <Gamehubbro@mail.ru>
2025-11-04 16:04:43 +0100

    Update config.py - added UTM marks

--- JustDoTattoo_bot/config.py
+++ JustDoTattoo_bot/config.py
@@ -1,15 +1,17 @@
 BOT_TOKEN = "8125363291:AAFk6cm2AXfwTe3fA8SkUW8A8KJGahhLmHM"
-BASE_URL = "https://justdotattoo.ru"
-IDEAS_URL = "https://t.me/tattooideas_justdotattoo"
+BASE_URL = "https://justdotattoo.ru/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+GALLERY_URL = "https://justdotattoo.ru/gallery/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+IDEAS_URL = "https://t.me/tattooideas_justdotattoo/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
 
-ARTICLES_URL = "https://justdotattoo.ru/articles"
-ARTICLE_1_URL = "https://justdotattoo.ru/articles/stoit-li-delat-tatuirovku/"
-ARTICLE_2_URL = "https://justdotattoo.ru/articles/idealnye-zony-dlya-muzhskoy-tatuirovki/"
-ARTICLE_3_URL = "https://justdotattoo.ru/articles/pervaya-tatuirovka/"
-ARTICLE_4_URL = "https://justdotattoo.ru/articles/tatuirovki-i-karera/"
-ARTICLE_5_URL = "https://justdotattoo.ru/articles/skolko-chasov-ukhodit-na-sozdanie-tatuirovki/"
-ARTICLE_6_URL = "https://justdotattoo.ru/articles/ukhod-za-tatu-posle-nabitiya-s-plyenkoy/"
-ARTICLE_7_URL = "https://justdotattoo.ru/articles/10-idej-intimnyh-tatuirovok/"
+ARTICLES_URL = "https://justdotattoo.ru/articles/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_1_URL = "https://justdotattoo.ru/articles/stoit-li-delat-tatuirovku/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_2_URL = "https://justdotattoo.ru/articles/idealnye-zony-dlya-muzhskoy-tatuirovki/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_3_URL = "https://justdotattoo.ru/articles/pervaya-tatuirovka/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_4_URL = "https://justdotattoo.ru/articles/tatuirovki-i-karera/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_5_URL = "https://justdotattoo.ru/articles/skolko-chasov-ukhodit-na-sozdanie-tatuirovki/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_6_URL = "https://justdotattoo.ru/articles/ukhod-za-tatu-posle-nabitiya-s-plyenkoy/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+ARTICLE_7_URL = "https://justdotattoo.ru/articles/10-idej-intimnyh-tatuirovok/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
 
 
-STUDIOS_URL = "https://justdotattoo.ru/studios"
+STUDIOS_URL = "https://justdotattoo.ru/studios/?utm_source=tg&utm_medium=bot&utm_campaign=tattoo_bot"
+
